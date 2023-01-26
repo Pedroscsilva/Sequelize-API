2834dd4f2c11aa31cfb810c000ec0d4fce89154b
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-01-26 19:41:10 -0300

    feat: req07 - adding a model for categories table

---
+++ src/models/Category.js
@@ -0,0 +1,14 @@
+const CategorySchema = (sequelize, DataTypes) => {
+  const CategoryTable = sequelize.define('Category', {
+   id: { type: DataTypes.INTEGER, primaryKey: true },
+   name: DataTypes.STRING,
+  }, {
+   tableName: 'categories',
+   underscored: true,
+   timestamps: false,
+  })
+
+  return CategoryTable;
+};
+
+module.exports = CategorySchema;
