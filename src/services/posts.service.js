cc3609a550d4052633b3da7e7807a21dbe4be731
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-01-26 22:37:20 -0300

    feat: req12 - adding support for an unmanaged transaction

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -1,27 +1,37 @@
-// const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 const { BlogPost, PostCategory } = require('../models');
 const { decodeToken } = require('../utils/JWT');
 const { checkPost } = require('./validations/inputValidations');
+
+const config = require('../config/config');
+
+const env = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[env]);
 
 const createPost = async (postObject, token) => {
   await checkPost(postObject);
   const { id } = decodeToken(token);
 
-  const fullObject = {
-    ...postObject,
-    userId: id,
-  };
+  const fullObject = { ...postObject, userId: id };
 
-  const newPost = await BlogPost.create(fullObject);
+  const t = await sequelize.transaction();
 
-  const postCategoryArray = postObject.categoryIds.map((e) => ({
-    categoryId: e, 
-    postId: newPost.dataValues.id,
-  }));
+  try {
+    const newPost = await BlogPost.create(fullObject, { transaction: t });
 
-  await PostCategory.bulkCreate(postCategoryArray);
+    const postCategoryArray = postObject.categoryIds.map((e) => ({
+      categoryId: e, 
+      postId: newPost.dataValues.id,
+    }));
+  
+    await PostCategory.bulkCreate(postCategoryArray, { transaction: t });
+    await t.commit();
 
-  return newPost.dataValues;
+    return newPost.dataValues;
+  } catch (e) {
+    await t.rollback(); 
+    throw e;
+  }
 };
 
 module.exports = {
